83843362c2ba86563ca6be8e9da7b57035238b08
Taf Makura <tafmakura@gmail.com>
2025-05-23 16:45:11 +0200

    Update class-setup.php

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -28,7 +28,7 @@ class Setup {
         require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-shortcodes.php';
 
         // Core Functions 
-        require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/functions.php';
+        require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/functions-admin.php';
     }
 
     /**

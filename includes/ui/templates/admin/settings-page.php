5ff334b5e0e3f5ad692ac5faa0bfb48da9172754
Taf Makura <tafmakura@gmail.com>
2025-05-23 22:36:08 +0200

    Update settings-page.php

--- includes/ui/templates/admin/settings-page.php
+++ includes/ui/templates/admin/settings-page.php
@@ -14,8 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     <h1><?php echo esc_html($page_title); ?></h1>
     <form method="post" action="options.php">
         <?php
-        settings_fields('arsol_css_addons_options'); // Changed to match register_setting
-        do_settings_sections('arsol_css_addons_options'); // Changed to match add_settings_section
+        settings_fields('arsol-css-addons');
+        do_settings_sections('arsol-css-addons');
         submit_button();
         ?>
     </form>

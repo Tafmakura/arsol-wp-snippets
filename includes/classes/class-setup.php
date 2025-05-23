7ebd123e613400edb39329e3279235f7e82ac4f5
Taf Makura <tafmakura@gmail.com>
2025-05-23 13:57:43 +0200

    Update class-setup.php

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -15,28 +15,27 @@ class Setup {
 
     public function init() {
         // Load plugin text domain
-        load_plugin_textdomain('arsol-css-addons', false, dirname(ARSOL_CSS_ADDONS_PLUGIN_BASENAME) . '/languages');
+        // load_plugin_textdomain('arsol-css-addons', false, dirname(ARSOL_CSS_ADDONS_PLUGIN_BASENAME) . '/languages');
     }
 
     /**
      * Include necessary files.
-     *//*
-    private function require_files() 
+     */
+    private function require_files() {
         // Core Classes
-        require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-assets.php';
-        require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-admin-settings.php';
-        require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-shortcodes.php';
+       // require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-assets.php';
+       // require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-admin-settings.php';
+       // require_once ARSOL_CSS_ADDONS_PLUGIN_DIR . 'includes/classes/class-shortcodes.php';
     }
 
     /**
      * Instantiate plugin classes.
-     *//*
+     */
     private function instantiate_classes() {
-        new \Arsol_CSS_Addons\Assets();
-        new \Arsol_CSS_Addons\Admin_Settings();
-        new \Arsol_CSS_Addons\Shortcodes();
+     //   new \Arsol_CSS_Addons\Assets();
+     //   new \Arsol_CSS_Addons\Admin_Settings();
+     //   new \Arsol_CSS_Addons\Shortcodes();
     }
-        */
 }
 
 // Initialize the setup class

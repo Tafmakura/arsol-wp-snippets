4276b3be85f7b9a0ba978819bef6d14aa52377ac
Taf Makura <tafmakura@gmail.com>
2025-05-23 17:12:56 +0200

    Update class-admin-settings.php

--- includes/classes/class-admin-settings.php
+++ includes/classes/class-admin-settings.php
@@ -211,15 +211,33 @@ class Admin_Settings {
         }
         
         foreach ($available_frontend_css as $css_id => $css_data) {
-            $checked = isset($frontend_css_options[$css_id]) ? $frontend_css_options[$css_id] : 0;
-            ?>
-            <p>
-                <input type="checkbox" id="arsol-frontend-css-<?php echo esc_attr($css_id); ?>" 
-                       name="arsol_css_addons_options[frontend_css_options][<?php echo esc_attr($css_id); ?>]" 
-                       value="1" <?php checked(1, $checked); ?>/>
-                <label for="arsol-frontend-css-<?php echo esc_attr($css_id); ?>"><?php echo esc_html($css_data['name']); ?></label>
-            </p>
-            <?php
+            // Check if file exists by converting URL to file path
+            $file_url = $css_data['file'];
+            $file_path = str_replace(ARSOL_CSS_ADDONS_PLUGIN_URL, ARSOL_CSS_ADDONS_PLUGIN_DIR, $file_url);
+            $file_exists = file_exists($file_path);
+            
+            if ($file_exists) {
+                // File exists - show checkbox
+                $checked = isset($frontend_css_options[$css_id]) ? $frontend_css_options[$css_id] : 0;
+                ?>
+                <p>
+                    <input type="checkbox" id="arsol-frontend-css-<?php echo esc_attr($css_id); ?>" 
+                           name="arsol_css_addons_options[frontend_css_options][<?php echo esc_attr($css_id); ?>]" 
+                           value="1" <?php checked(1, $checked); ?>/>
+                    <label for="arsol-frontend-css-<?php echo esc_attr($css_id); ?>"><?php echo esc_html($css_data['name']); ?></label>
+                </p>
+                <?php
+            } else {
+                // File doesn't exist - show error message
+                ?>
+                <p class="arsol-css-error">
+                    <span class="dashicons dashicons-warning" style="color: #d63638; vertical-align: middle;"></span>
+                    <span style="color: #d63638;">
+                        <?php echo esc_html(sprintf(__('CSS file "%s" could not be found.', 'arsol-css-addons'), $css_data['name'])); ?>
+                    </span>
+                </p>
+                <?php
+            }
         }
     }
     

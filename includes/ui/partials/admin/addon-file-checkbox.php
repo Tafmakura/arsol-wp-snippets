18db6f6a3bf8ef8bf7b366257db63333bc05ff46
Taf Makura <tafmakura@gmail.com>
2025-05-24 20:08:38 +0200

    Update addon-file-checkbox.php

--- includes/ui/partials/admin/addon-file-checkbox.php
+++ includes/ui/partials/admin/addon-file-checkbox.php
@@ -15,10 +15,32 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Check if file exists by converting URL to file path
-$file_url = $addon_data['file'];
-$file_path = str_replace(ARSOL_WP_SNIPPETS_PLUGIN_URL, ARSOL_WP_SNIPPETS_PLUGIN_DIR, $file_url);
-$file_exists = file_exists($file_path);
+// Check if file exists
+$file_exists = true;
+$file_reference = $addon_data['file'];
+
+// Determine how to check file existence based on file type
+if (filter_var($file_reference, FILTER_VALIDATE_URL)) {
+    // It's a URL - convert to file path for theme files
+    $theme_child_uri = get_stylesheet_directory_uri();
+    $theme_parent_uri = get_template_directory_uri();
+    
+    if (strpos($file_reference, $theme_child_uri) === 0) {
+        // Child theme file
+        $file_path = str_replace($theme_child_uri, get_stylesheet_directory(), $file_reference);
+        $file_exists = file_exists($file_path);
+    } elseif (strpos($file_reference, $theme_parent_uri) === 0) {
+        // Parent theme file
+        $file_path = str_replace($theme_parent_uri, get_template_directory(), $file_reference);
+        $file_exists = file_exists($file_path);
+    } else {
+        // External URL or plugin file - assume it exists
+        $file_exists = true;
+    }
+} else {
+    // It's a file path - check directly
+    $file_exists = file_exists($file_reference);
+}
 
 if ($file_exists) {
     // File exists - show checkbox
@@ -32,13 +54,14 @@ if ($file_exists) {
     </p>
     <?php
 } else {
-    // File doesn't exist - show error message with file path
+    // File doesn't exist - show error message
     ?>
     <p class="arsol-addon-error">
         <span class="dashicons dashicons-warning" style="color: #d63638; vertical-align: middle;"></span>
         <span style="color: #d63638;">
-            <?php echo esc_html(sprintf(__('Addon file for "%s" could not be found at: %s', 'arsol-wp-snippets'), $addon_data['name'], $file_path)); ?>
+            <?php echo esc_html(sprintf(__('Addon file for "%s" could not be found at: %s', 'arsol-wp-snippets'), $addon_data['name'], isset($file_path) ? $file_path : $file_reference)); ?>
         </span>
     </p>
     <?php
 }
+?>

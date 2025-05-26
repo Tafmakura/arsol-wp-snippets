a3b5edfadd3bebf168cccff050af8c68c3ee1f54
Taf Makura <tafmakura@gmail.com>
2025-05-26 18:48:11 +0200

    Update class-admin-settings.php

--- includes/classes/class-admin-settings.php
+++ includes/classes/class-admin-settings.php
@@ -198,12 +198,10 @@ class Admin_Settings {
          */
         $filtered_options = apply_filters('arsol_wp_snippets_css_addon_files', $css_addon_options);
         
-        // Validate that all files are CSS files and exist
+        // Validate that all files are CSS files
         foreach ($filtered_options as $addon_id => $addon_data) {
             // Check if file path exists and ends with .css
-            if (!isset($addon_data['file']) || 
-                substr($addon_data['file'], -4) !== '.css' || 
-                !file_exists($addon_data['file'])) {
+            if (!isset($addon_data['file']) || substr($addon_data['file'], -4) !== '.css') {
                 // Remove invalid entries
                 unset($filtered_options[$addon_id]);
             }
@@ -226,12 +224,10 @@ class Admin_Settings {
          */
         $filtered_options = apply_filters('arsol_wp_snippets_js_addon_files', $js_addon_options);
         
-        // Validate that all files are JS files and exist
+        // Validate that all files are JS files
         foreach ($filtered_options as $addon_id => $addon_data) {
             // Check if file path exists and ends with .js
-            if (!isset($addon_data['file']) || 
-                substr($addon_data['file'], -3) !== '.js' || 
-                !file_exists($addon_data['file'])) {
+            if (!isset($addon_data['file']) || substr($addon_data['file'], -3) !== '.js') {
                 // Remove invalid entries
                 unset($filtered_options[$addon_id]);
             }
